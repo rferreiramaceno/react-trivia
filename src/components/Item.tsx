7811de9d8e121610e71f6bdde8c38295f6b61b4b
rferreiramaceno <rferreiramaceno@gmail.com>
2024-06-18 19:25:57 -0600

    All components are working properly. Aadded all 20 questions and answers.

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
+
 
 interface Props {
     children: string;
     onClick: () => void;
 }
 
-function Item({children, onClick}: Props) {
-    
-    return <div id="item" onClick={onClick} >{children}</div>
+function Item({children, onClick}: Props) { 
+
+        return <div id="item" onClick={onClick}>{children}</div>  
+
 }
 
 export default Item;
